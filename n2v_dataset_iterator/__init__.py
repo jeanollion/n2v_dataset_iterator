79589453bcac07d271612a9c7936fb75af6f74f3
jean.ollion <jean.ollion@polytechnique.org>
2020-03-25 10:12:39 +0100

    added pm_null to ini

--- n2v_dataset_iterator/__init__.py
+++ n2v_dataset_iterator/__init__.py
@@ -1,2 +1,2 @@
-from .value_manipulators import pm_normal_withoutCP, pm_uniform_withCP, pm_normal_additive, pm_normal_fitted, pm_identity
+from .value_manipulators import pm_null, pm_normal_withoutCP, pm_uniform_withCP, pm_normal_additive, pm_normal_fitted, pm_identity
 from .manipulate_data import manipulate_data, manipulate_data_fun
